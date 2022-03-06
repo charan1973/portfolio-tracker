9c2a2bb084d02b3685392a2c65828e84203dbcdf
charan1973 <v.vicky1973@gmail.com>
2022-03-06 20:29:31 +0530

    update: configured swagger to run apis

--- config/index.js
+++ config/index.js
@@ -3,11 +3,15 @@ require('dotenv').config();
 const {
   DATABASE_URL,
   PORT,
+  HOST_URL,
+  NODE_ENV,
 } = process.env;
 
 const envVariables = {
+  node_env: NODE_ENV,
   port: Number(PORT) || 3000,
   db_url: DATABASE_URL,
+  host_url: HOST_URL,
 };
 
 module.exports = {
--- config/swagger.js
+++ config/swagger.js
@@ -1,6 +1,13 @@
 const swaggerAutogen = require('swagger-autogen')();
+const { envVariables } = require('.');
 
 const outputFile = './temp/swagger_output.json';
 const endpointsFiles = ['./app/routes/index.js'];
 
-swaggerAutogen(outputFile, endpointsFiles);
+const swaggerConfig = {
+  host: envVariables.host_url,
+  basePath: '/api/',
+  schemes: envVariables.host_url === 'production' ? 'https' : 'http',
+};
+
+swaggerAutogen(outputFile, endpointsFiles, swaggerConfig);
